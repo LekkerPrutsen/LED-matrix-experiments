65de01f7bc53549a1bd4d717872cb2a6c987b922
LekkerPrutsen <info@lekkerprutsen.nl>
2016-12-29 15:04:11 +0100

    First version

---
+++ LED_identify_devices_1.py
@@ -0,0 +1,22 @@
+import time
+import max7219.led as led
+
+device = led.matrix(cascaded=4)
+
+device.letter(0, ord('1'))
+time.sleep(1)
+
+device.letter(1, ord('2'))
+time.sleep(1)
+
+device.letter(2, ord('3'))
+time.sleep(1)
+
+device.letter(3, ord('4'))
+time.sleep(1)
+
+
+
+
+
+
